eaea743b41dea36b89e4a6ad4635d31d4eb3a48f
Pratik Somwanshi <vicky.somwanshi@gmail.com>
2021-12-07 15:55:06 +0530

    Update Day01.py

--- Day01.py
+++ Day01.py
@@ -1,22 +1,29 @@
 def has_increased(arr):
     return sum(arr[:3]) < sum(arr[1:])
 
-def main():
-    # Part 1
+def part1(numbers):
     count, prev = -1, 0
-    numbers = list(map(int, open('./Day01.txt', 'r').readlines()))
     for num in numbers:
         if num > prev:
             count += 1
         prev = num
-    print(count)
-
-    # Part 2
+    print("Part1 answer: ", count)
+    
+def part2(numbers):
     count = 0
     for i in range(len(numbers)-3):
         if has_increased(numbers[i:i+4]):
             count += 1
-    print(count)
+    print("Part2, answer:", count)
+
+def main():
+    numbers = list(map(int, open('./Day01.txt', 'r').readlines()))
+    
+    # Part 1
+    part1()
+
+    # Part 2
+    part2()
 
 if __name__ == "__main__":
     main()
