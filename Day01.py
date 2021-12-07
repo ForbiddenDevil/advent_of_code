83189775066d284526ebc10ebedc12a352c4516b
Pratik Somwanshi <vicky.somwanshi@gmail.com>
2021-12-07 15:35:09 +0530

    Update Day01.py

--- Day01.py
+++ Day01.py
@@ -1,9 +1,21 @@
+def has_increased(arr):
+    return sum(arr[:3]) < sum(arr[1:])
+
 def main():
+    # Part 1
     count, prev = -1, 0
-    for num in list(map(int, open('./Day01.txt', 'r').readlines())):
+    numbers = list(map(int, open('./Day01.txt', 'r').readlines()))
+    for num in numbers:
         if num > prev:
             count += 1
         prev = num
+    print(count)
+
+    # Part 2
+    count = 0
+    for i in range(len(numbers)-3):
+        if has_increased(numbers[i:i+4]):
+            count += 1
     print(count)
 
 if __name__ == "__main__":
